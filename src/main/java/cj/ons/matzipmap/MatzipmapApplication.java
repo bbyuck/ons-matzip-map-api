f0f3a3a3a6df68d7ff8ccd15a7d791feafb4590b
hyuk <hyuk.kang1@cj.net>
2022-11-18 09:06:05 +0900

    Project start

---
+++ src/main/java/cj/ons/matzipmap/MatzipmapApplication.java
@@ -0,0 +1,13 @@
+package cj.ons.matzipmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatzipmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatzipmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cj/ons/matzipmap/MatzipmapApplicationTests.java
@@ -0,0 +1,13 @@
+package cj.ons.matzipmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatzipmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
